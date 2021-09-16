4fd2b296ea1a708fbd100d92f80e8433b19500f0
bestPeople105 <89798394+bestPeople105@users.noreply.github.com>
2021-09-16 12:44:26 +0700

    Update main.py

--- main.py
+++ main.py
@@ -53,4 +53,4 @@ async def changestatus(ctx, botStatus, *, reason):
 	if botStatus == 'dnd':
 		await bot.change_presence(status = discord.Status.dnd, activity = discord.Activity(type = discord.ActivityType.listening, name = f'{reason}'))
 
-bot.run('NzQ1Mjk0ODYwODM5NDIwMDM0.Xzvr7Q.xmn-I499E7R_4GT9nncGUy0ueN8')
+bot.run('NzQ1Mjk0ODYwODM5NDIwMDM0.Xzvr7Q.YDgBpbbg85mMCGutU9-6q1QV-os')
