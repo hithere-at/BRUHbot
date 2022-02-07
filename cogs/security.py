c736a074d50671c9269f4dc99cdc93f9693b6694
bestPeople <89798394+bestpeople105@users.noreply.github.com>
2022-02-07 15:09:49 +0700

    Update security.py

--- cogs/security.py
+++ cogs/security.py
@@ -18,8 +18,8 @@ dns.resolver.default_resolver=dns.resolver.Resolver(configure=False)
 dns.resolver.default_resolver.nameservers=['8.8.8.8']
 
 client = pymongo.MongoClient(f'{os.getenv("MONGODB_URI")}')
-db = client["bruh"]
-stuff = db["nice"]
+db = client["money"]
+stuff = db["zen"]
 
 class security(commands.Cog):
 
